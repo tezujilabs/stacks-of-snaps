f9475c8ef9f1f5fbc93c85c0dfb5126b6c2fc8c4
Hoc Lie <hoclie@outlook.com>
2026-02-17 21:14:56 +1100

    initial revision.

---
+++ StacksOfSnaps/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace StacksOfSnaps
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
